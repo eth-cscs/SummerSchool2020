afaf660aecb9d0970eb98a15287ff7e9db0be30a
bcumming <bcumming@cscs.ch>
2020-07-12 16:31:08 +0200

    update plotting script

--- miniapp/openmp/plotting.py
+++ miniapp/openmp/plotting.py
@@ -7,11 +7,14 @@
 # Date: Thu May 21 14:11:38 CEST 2015
 #
 
-# check the DISPLAY environment variable
-# if it is not defined, don't show the image
-import os
-if os.environ.get('DISPLAY') == None:
-    import matplotlib
+import argparse
+import matplotlib
+
+P = argparse.ArgumentParser(description='plot miniapp and diffusion output')
+P.add_argument('-s', '--show', action='store_true',
+                   help='show result in popup window (requries X-forwarding)')
+args = P.parse_args()
+if not args.show:
     matplotlib.use('Agg')
 
 import pylab as pl
@@ -26,11 +29,9 @@ headerfile.close()
 rawFilename = header[1].split()[1]
 
 res_x, res_y = [int(x) for x in header[2].strip().split()[1:3]]
-print('xdim %s' % res_x)
-print('ydim %s' % res_y)
+print('xdim', res_x, ' ydim', res_y)
 
 data = np.fromfile(rawFilename, dtype=np.double, count=res_x*res_y, sep='')
-#assert data.shape[0] == res_x * res_y, "raw data array does not match the resolution in the header"
 
 x = np.linspace(0., 1., res_x)
 y = np.linspace(0., 1.*res_y/res_x, res_y)
@@ -39,9 +40,9 @@ X, Y = np.meshgrid(x, y)
 # number of contours we wish to see
 V = [-0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.01]
 pl.contourf(X, Y, data.reshape(res_y, res_x), V, alpha=.75, cmap='jet')
-pl.contour(X, Y, data.reshape(res_y, res_x), V, colors='black', linewidth=.01)
+pl.contour(X, Y, data.reshape(res_y, res_x), V, colors='black')
 
-#pl.clabel(C, inline=1)
 pl.axes().set_aspect('equal')
 pl.savefig("output.png", dpi=72)
-pl.show()
+if args.show:
+    pl.show()
--- miniapp/openmp/readme.md
+++ miniapp/openmp/readme.md
@@ -9,7 +9,7 @@ module swap PrgEnv-cray PrgEnv-gnu
 make -j4
 
 # to run without an allocation
-srun -Cgpu -n1 -c12 --hint=nomultithread --reservation=summer_school ./main 128 128 128 0.01
+srun -Cgpu -n1 -c12 --hint=nomultithread --reservation=summer_school ./main 128 128 100 0.01
 
 # to run with an interactive session
 salloc -Cgpu --reservation=summer_school
@@ -20,15 +20,22 @@ module load PyExtensions/python3-CrayGNU-19.10
 python3 plotting.py
 ```
 
-Benchmark results on Piz Daint, measured in CG iterations/second:
+Benchmark results on Piz Daint, measured in CG iterations/second, executed with:
+
+```
+OMP_NUM_THREADS=$nt srun -n1 -c12 --hint=nomultithread ./main $dim $dim 200 0.01
+```
+
+Where `$nt` is the number of threads, and `$dim` is the spatial dimsnsion.
+
 
 ```
            -------------------------------------------------------------
            | 1 thread | 2 threads | 4 threads | 8 threads | 12 threads |
 ------------------------------------------------------------------------
-|  64x 64  | ?        | ?         | ?         | ?         | ?          |
-| 256x256  | ?        | ?         | ?         | ?         | ?          |
-| 512x512  | ?        | ?         | ?         | ?         | ?          |
+| 128x128  | 7222.0   | 12959.1   | 20225.1   | 26231.0   | 26095.7    |
+| 256x256  | 1742.6   |  3250.2   |  6823.0   | 12021.4   | 15036.6    |
+| 512x512  |  454.8   |  1047.8   |  1929.1   |  3409.4   |  4544.6    |
 ------------------------------------------------------------------------
 
 ```
