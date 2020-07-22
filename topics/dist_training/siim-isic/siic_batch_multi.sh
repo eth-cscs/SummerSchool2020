bd9c1fb4b073962a6e72904c167b2dc6622ba60d
Henrique Mendonça <henrique@apache.org>
2020-07-22 14:32:37 +0200

    changes for student account

--- topics/dist_training/siim-isic/siic_batch_multi.sh
+++ topics/dist_training/siim-isic/siic_batch_multi.sh
@@ -4,6 +4,7 @@
 ## #SBATCH --reservation=summer_school
 #SBATCH --time=00:15:00
 #SBATCH --nodes=2
+#SBATCH --switches=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=24
